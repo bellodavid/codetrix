ad7421d44bc8adbdcf2d448a2937dc3e8a455d24
David Bello <davidbello998@gmail.com>
2022-11-11 13:08:22 -0800

    installed dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
-import React, {useState, useEffect } from 'react'
-import Editor from "./Editor"
-import useLocalStorage from '../hooks/useLocalStorage';
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 function App() {
-
-  const [html, setHtml] =useLocalStorage('html', '');
-  const [css, setCss] = useLocalStorage('css', '');
-  const [js, setJs] = useLocalStorage('js', '');
-  const [srcDoc, setSrcDoc] = useState('');
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setSrcDoc(`
-      <html>
-      <body>${html}</body>
-      <style>${css}</style>
-      <script>${js}</script>`)
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
     }, 250)
 
     return () => clearTimeout(timeout)
@@ -23,40 +24,38 @@ function App() {
 
   return (
     <>
-    <div className="pane top-pane">
-      <Editor
-      language="xml"
-      displayName="HTML"
-      value={html}
-      onChange={setHtml}
-      />
-      <Editor
-      language="css"
-      displayName="CSS"
-      value={css}
-      onChange={setCss}
-      />
-      <Editor
-      language="javascript"
-      displayName="Javascript"
-      value={js}
-      onChange={js}
-      />
-    
-    </div>
-    <div className="pane">
-      <iframe
-      srcDoc={srcDoc}
-      title= "output"
-      sandbox= "allow-scripts"
-      frameBorder="0"
-      width="100%"
-      height="100%"
-      
-      />
-    </div>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
